fe8ffb84d4f5e5094b13eda8243ea89e7664747a
FDR <delrincon@gmail.com>
2022-03-30 22:45:00 -0300

    change theme using cookie + final commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,60 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { lightTheme, darkTheme, customTheme } from '../themes'
+import '../styles/globals.css';
+import type { AppContext, AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material';
+
+import { lightTheme, darkTheme, customTheme } from '../themes';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+
+
+interface Props extends AppProps {
+    theme: string;
+}
+
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+
+    const [currentTheme, setCurrentTheme] = useState( lightTheme );
+
+    useEffect(() => {
+        const cookieTheme = Cookies.get('theme') || 'light'
+        
+        const selectedTheme = cookieTheme === 'light'
+            ? lightTheme 
+            : ( cookieTheme === 'dark')
+                ? darkTheme
+                : customTheme;
+
+        setCurrentTheme( selectedTheme );
+    }, [])
+    
+
+
+
+
     return (
-        <ThemeProvider theme={ darkTheme }>
-            <CssBaseline /> {/* es como normalize */}
+        <ThemeProvider theme={ currentTheme }>
+            <CssBaseline /> {/* es como normalize, pero sin esto no funcionan los temas */}
             <Component {...pageProps} />
         </ThemeProvider>
     )
 }
+
+// MyApp.getInitialProps = async ( appContext: AppContext ) => {
+
+//     const { theme } = appContext.ctx.req ? ( appContext.ctx.req as any ).cookies : { theme: 'light' }
+    
+//     const validThemes = ['light', 'dark', 'custom'];
+
+//     return {
+//         theme: validThemes.includes(theme) ? theme : 'dark',
+//     }
+
+
+// }
+
+
+
 
 export default MyApp
